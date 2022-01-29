6052d7f59f941f15c168919bb17a612b5f75972d
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2022-01-29 13:00:49 +0000

    adding grey

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function greenf(cell) {
     document.getElementById(cell).style.border = green;
 }
 
+function grayf(cell) {
+    document.getElementById(cell).style.backgroundColor = gray;
+    document.getElementById(cell).style.border = gray;
+}
 const array = ["p", "a", "y", "e", "e"];
 let enter;
 let a;
@@ -18,16 +22,21 @@ let e;
 let f;
 const orange = "#b59d3b"
 const green = "#538d4e"
+const gray = "#4f4f52"
 
 function display(rows) {
-    enter = prompt("enter guess 1");
-    document.getElementById(rows + '1').innerHTML = enter[0];
-    document.getElementById(rows + '2').innerHTML = enter[1];
-    document.getElementById(rows + '3').innerHTML = enter[2];
-    document.getElementById(rows + '4').innerHTML = enter[3];
-    document.getElementById(rows + '5').innerHTML = enter[4];
-    check(rows, enter)
-    a = "y"
+    enter = prompt("enter a 5 letter word for row: " + rows);
+    if (enter.length == 5) {
+        document.getElementById(rows + '1').innerHTML = enter[0];
+        document.getElementById(rows + '2').innerHTML = enter[1];
+        document.getElementById(rows + '3').innerHTML = enter[2];
+        document.getElementById(rows + '4').innerHTML = enter[3];
+        document.getElementById(rows + '5').innerHTML = enter[4];
+        check(rows, enter)
+        a = "y"
+    } else {
+        alert("must be 5 letters long")
+    }
 }
 
 function check(row, content) {
@@ -42,7 +51,7 @@ function check(row, content) {
         } else if (array.includes(content[i - 1]) == true) {
             orangef(row + String(i));
         } else {
-
+            grayf(row + String(i));
         }
         i += 1;
     }
