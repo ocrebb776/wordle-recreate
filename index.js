9132f4ac30066e1232f18771930f85130922cc06
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2022-01-30 16:36:58 +0000

    bug

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function hidden() {
 }
 function notlong() {
   document.getElementById('fe').style.width = '60vw'
+  document.getElementById('fe').style.hight = '60vh'
   document.getElementById('fe').style.fontSize = '5vh'
   delay(2).then(() => hidden())
 }
